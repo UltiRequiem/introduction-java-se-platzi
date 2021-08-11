8217ec545c14c52bba48440d5972c5da149b62d3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-11 11:46:05 -0500

    :sparkles: Number data types

---
+++ src/NumTypes.java
@@ -0,0 +1,29 @@
+public class NumTypes {
+  public static void main(String[] args) {
+    System.out.println("bits tipo byte:" + Byte.SIZE);
+    System.out.println("bytes tipo byte:" + Byte.BYTES);
+    System.out.println("valor minimo tipo byte:" + Byte.MIN_VALUE);
+    System.out.println("valor maximo tipo byte:" + Byte.MAX_VALUE);
+
+    System.out.println("bits tipo short:" + Short.SIZE);
+    System.out.println("bytes tipo short:" + Short.BYTES);
+    System.out.println("valor minimo tipo short:" + Short.MIN_VALUE);
+    System.out.println("valor maximo tipo short:" + Short.MAX_VALUE);
+
+    System.out.println("bits tipo int:" + Integer.SIZE);
+    System.out.println("bytes tipo int:" + Integer.BYTES);
+    System.out.println("valor minimo tipo int:" + Integer.MIN_VALUE);
+    System.out.println("valor maximo tipo int:" + Integer.MAX_VALUE);
+
+    System.out.println("bits tipo long:" + Long.SIZE);
+    System.out.println("bytes tipo long:" + Long.BYTES);
+    System.out.println("valor minimo tipo long:" + Long.MIN_VALUE);
+    System.out.println("valor maximo tipo long:" + Long.MAX_VALUE);
+
+    byte byteVar = 15;
+    short shortVar = 100;
+    int intVar = 500;
+    long longVar = 1000;
+    var numb = 100;
+  }
+}
