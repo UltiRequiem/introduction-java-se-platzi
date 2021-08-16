8df5621967c9fa8f67a40e1285f1b51f1ef8349b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-16 11:41:46 -0500

    :sparkles: Conditionals

---
+++ src/IsStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement{
+  public static void main(String[] args) {
+    boolean isBluetoothEnabled = true;
+    int fileSended = 3;
+
+    if (isBluetoothEnabled){
+      // Send File
+      fileSended++;
+      System.out.println("File Sended!");
+    }
+  }
+}
