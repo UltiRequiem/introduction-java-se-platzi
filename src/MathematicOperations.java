0a3f84b2b45c4282cb89cdb4704cdb468d66bd57
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-16 10:24:19 -0500

    :sparkles: Math

---
+++ src/MathematicOperations.java
@@ -0,0 +1,31 @@
+public class MathematicOperations {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        double i = 2.1;
+        double j = 3.5;
+
+        System.out.println(Math.ceil(i));
+
+        System.out.println(Math.floor(j));
+
+        System.out.println(Math.max(a,b));
+
+        System.out.println(Math.pow(a,b));
+
+        System.out.println(Math.abs(j));
+
+        System.out.println(Math.addExact(a,b));
+
+        System.out.println(Math.atan(j));
+
+        System.out.println(Math.cbrt(i));
+
+        System.out.println(Math.cosh(j));
+
+        System.out.println(Math.cos(j));
+
+        System.out.println(Math.exp(i));
+    }
+}
