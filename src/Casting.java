7c4e000093f0f63167c7e08f9c40dd4a0d1e02f9
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-16 11:03:01 -0500

    :sparkles: Casting

---
+++ src/Casting.java
@@ -0,0 +1,11 @@
+public class Casting{
+  public static void main(String[] args) {
+
+    double monthlyDogs = 30.0/12.0;
+    System.out.println(monthlyDogs);
+
+    // Estimation
+    int estimatedMonthlyDogs =(int) monthlyDogs;
+    System.out.println(estimatedMonthlyDogs);
+  }
+}
