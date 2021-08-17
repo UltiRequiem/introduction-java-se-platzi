e7a3e35737d527deb765cf72a0ec6d6d84bdcadc
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-17 14:14:33 -0500

    :sparkles: Finalization

---
+++ src/Arrays.java
@@ -0,0 +1,7 @@
+public class Arrays {
+  public static void main(String[] args) {
+    String[] androidVersions = new String[17];
+    String days[] = new String[7];
+    String[][] cities = new String[4][2];
+  }
+}
