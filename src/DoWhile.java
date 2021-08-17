e71fd9fe436c63a13e99ccd344edc21fc2ad496e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-17 08:53:14 -0500

    :sparkles: While

---
+++ src/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      System.out.println(i);
+      i++;
+    } while (i < 5);
+  }
+}
---
+++ src/While.java
@@ -0,0 +1,9 @@
+public class While {
+  public static void main(final String[] args) {
+    int i = 0;
+    while (i < 10) {
+      i++;
+      System.out.println(i);
+    }
+  }
+}
